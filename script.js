a0c5b75e0ec755cf0ce88223ad6403a0d1dbe04d
Raissa Baeta <rbaetadev@gmail.com>
2022-11-08 16:09:15 -0300

    Adicionando link aos dias de jogos

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ function createCard(date, day, games) {
     return `
     <div class="card"
     style="animation-delay: ${delay}s">
-    <h2>${date} <span>${day}</span></h2>
+    <h2> ${date}
+    <a href="https://www.google.com/search?q=copa+do+mundo+calend%C3%A1rio+&hl=en&sxsrf=ALiCzsZoKWNkDcZB5r6yUZ_gOOckX6sfew%3A1666552817096&ei=8ZNVY5S4BZHM1sQPhYyY8AI&ved=0ahUKEwjU9KWFiff6AhURppUCHQUGBi4Q4dUDCA8&uact=5&oq=copa+do+mundo+calend%C3%A1rio+&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECcyCAgAEIAEEMsBMggIABCABBDLATIICAAQgAQQywEyCAgAEIAEEMsBMggIABCABBDLATIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeOgoIABBHENYEELADOg0IABDkAhDWBBCwAxgBSgQITRgBSgQIQRgASgQIRhgBUKgDWKgDYIkGaAFwAXgAgAGhAYgBoQGSAQMwLjGYAQCgAQHIAQ3AAQHaAQYIARABGAk&sclient=gws-wiz#sie=lg;/m/0fp_8fm;2;/m/030q7;mt;fp;1;;;" target="_blank"> <span>${day}</span></h2>
     <ul>
     ${games}
     </ul>
